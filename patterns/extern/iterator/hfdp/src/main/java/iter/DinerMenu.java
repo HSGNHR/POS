40000cf1c745003d31cf273bb83faa9e5ffcb434
take2 <take2@nessaja.me>
2017-05-18 17:29:20 +0200

    neue Datei:     extern/iterator/hfdp/.gitignore 	neue Datei:     extern/iterator/hfdp/build.gradle 	neue Datei:     extern/iterator/hfdp/src/main/java/iter/DinerMenu.java 	neue Datei:     extern/iterator/hfdp/src/main/java/iter/PancakeHouseMenu.java 	neue Datei:     extern/iterator/hfdp/src/main/java/iter/Start.java

---
+++ patterns/extern/iterator/hfdp/src/main/java/iter/DinerMenu.java
@@ -0,0 +1,24 @@
+package iter;
+
+public class DinerMenu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+    public DinerMenu(){
+       menuItems = new MenuItem[MAX_ITEMS] 
+    }
+    addItem("Vegetarian BLT", "Lettuce and Tomatoes on whole wheat.", true, 2.99);
+
+    public void addItem(String name, String description, boolean vegetarian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if(numberOfItems >= MAX_ITEMS){
+            System.out.println("Sorry, menu is full! Can't add more items");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+    public MenuItem[] getMenuItem(){
+        return menuItems;
+    }
+}
